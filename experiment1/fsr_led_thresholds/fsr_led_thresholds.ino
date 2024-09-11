90a855e70f5e3e64260f4cdf3160dd8aed2c0325
Nick Puckett <nick@puckettrand.com>
2024-09-11 16:17:26 -0400

    threshold update

--- experiment1/fsr_led_thresholds/fsr_led_thresholds.ino
+++ experiment1/fsr_led_thresholds/fsr_led_thresholds.ino
@@ -1,18 +1,23 @@
-const int numInputs = 5;
+/*
+analog threshold. When the value of the sensor goes above the corresponding threshold value, it lights up the led
+
+
+
+
+*/
+
+const int numInputs = 4;
 
 // Analog input pins
-const int analogInputPins[numInputs] = {A0, A1, A2, A3, A4};
-
+const int analogInputPins[] = {A0, A1, A2, A3, A4};
 // LED output pins
-const int ledPins[numInputs] = {2, 3, 4, 5, 6};
+const int ledPins[] = {2, 3, 4, 5, 6};
 
 // Individual thresholds for each analog input
-int threshold1 = 512;
-int threshold2 = 512;
-int threshold3 = 512;
-int threshold4 = 512;
-int threshold5 = 512;
-int threshold6 = 512;
+int thresholds[] = {512, 512, 512, 512, 512};
+
+// Boolean variables for LED states
+bool ledStates[] = {false, false, false, false, false};
 
 void setup() {
   Serial.begin(9600);
@@ -24,53 +29,29 @@ void setup() {
 }
 
 void loop() {
-  // Read analog inputs and control LEDs
-  int sensorValue1 = analogRead(analogInputPins[0]);
-  int sensorValue2 = analogRead(analogInputPins[1]);
-  int sensorValue3 = analogRead(analogInputPins[2]);
-  int sensorValue4 = analogRead(analogInputPins[3]);
-  int sensorValue5 = analogRead(analogInputPins[4]);
-  int sensorValue6 = analogRead(analogInputPins[5]);
+  for (int i = 0; i < numInputs; i++) {
+    // Read analog input
+    int sensorValue = analogRead(analogInputPins[i]);
+    
+    // Control LED based on threshold
+    if (sensorValue > thresholds[i]) {
+      ledStates[i] = true;
+      digitalWrite(ledPins[i], HIGH);
+    } else {
+      ledStates[i] = false;
+      digitalWrite(ledPins[i], LOW);
+    }
+    
+    // Print values to serial for calibration
+    Serial.print("Sensor ");
+    Serial.print(i + 1);
+    Serial.print(": ");
+    Serial.print(sensorValue);
+    Serial.print(" (Threshold: ");
+    Serial.print(thresholds[i]);
+    Serial.print(") | LED State: ");
+    Serial.println(ledStates[i]);
+  }
   
-  // Control LEDs based on thresholds
-  digitalWrite(ledPins[0], sensorValue1 > threshold1 ? HIGH : LOW);
-  digitalWrite(ledPins[1], sensorValue2 > threshold2 ? HIGH : LOW);
-  digitalWrite(ledPins[2], sensorValue3 > threshold3 ? HIGH : LOW);
-  digitalWrite(ledPins[3], sensorValue4 > threshold4 ? HIGH : LOW);
-  digitalWrite(ledPins[4], sensorValue5 > threshold5 ? HIGH : LOW);
-  digitalWrite(ledPins[5], sensorValue6 > threshold6 ? HIGH : LOW);
-  
-
-  
-  // Print values to serial for calibration
-  Serial.print("Sensor 1: ");
-  Serial.print(sensorValue1);
-  Serial.print(" (Threshold: ");
-  Serial.println(threshold1);
-
-  
-  Serial.print("Sensor 2: ");
-  Serial.print(sensorValue2);
-  Serial.print(" (Threshold: ");
-  Serial.println(threshold2);
-
-  
-  Serial.print("Sensor 3: ");
-  Serial.print(sensorValue3);
-  Serial.print(" (Threshold: ");
-  Serial.println(threshold3);
-
-  
-  Serial.print("Sensor 4: ");
-  Serial.print(sensorValue4);
-  Serial.print(" (Threshold: ");
-  Serial.println(threshold4);
-
-  
-  Serial.print("Sensor 5: ");
-  Serial.print(sensorValue5);
-  Serial.print(" (Threshold: ");
-  Serial.println(threshold5);
-
-
+  Serial.println("--------------------");
 }
